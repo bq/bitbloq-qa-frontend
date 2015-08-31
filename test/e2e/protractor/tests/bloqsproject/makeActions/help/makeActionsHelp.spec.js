e0caa2fc96e38e23531caf6e93ffa679677effd2
luisangonzalez <luisantonio.gonzalez@bq.com>
2015-08-31 12:42:56 +0200

    grunt-cli added as dev dependencies && id test testlink on makeActionsHelp
    Change-Id: Id59f22ab4b58ba4588350641e647c33a7677a0c5

--- test/e2e/protractor/tests/bloqsproject/makeActions/help/makeActionsHelp.spec.js
+++ test/e2e/protractor/tests/bloqsproject/makeActions/help/makeActionsHelp.spec.js
@@ -6,130 +6,129 @@
 'use strict';
 
 var Variables = require('../../../commons/variables.js'),
-    MakeActions = require('../makeActions.po.js'),
-    Login = require('../../../login/login.po.js'),
-    Make = require('../..//make.po.js'),
-    Modals = require('../../../modals/modals.po.js');
+  MakeActions = require('../makeActions.po.js'),
+  Login = require('../../../login/login.po.js'),
+  Make = require('../..//make.po.js'),
+  Modals = require('../../../modals/modals.po.js');
 
 var vars = new Variables(),
-    makeActions = new MakeActions(),
-    login = new Login(),
-    make = new Make(),
-    modals = new Modals();
+  makeActions = new MakeActions(),
+  login = new Login(),
+  make = new Make(),
+  modals = new Modals();
 
 describe('Menu Help of MakeActions', function() {
 
-    //beforeEach commons
-    vars.beforeTest();
+  //beforeEach commons
+  vars.beforeTest();
 
-    // afterEach commons
-    vars.afterTest();
+  // afterEach commons
+  vars.afterTest();
 
-    it('bba-94:Login and test if there are all items visibles && redirect to faq, forum and help', function() {
+  it('bba-94:Login and test if there are all items visibles && redirect to faq, forum and help', function() {
 
-        login.loginWithRandomUser();
-        make.get();
-        modals.rejectTour();
-        browser.sleep(vars.timeToWaitFadeModals);
-        makeActions.menuHelp.click();
+    login.loginWithRandomUser();
+    make.get();
+    modals.rejectTour();
+    browser.sleep(vars.timeToWaitFadeModals);
+    makeActions.menuHelp.click();
 
-        //test item "Enviar comentarios" && "Informar de un error"  are enable
-        expect(makeActions.menuHelpComments.getAttribute('disabled')).not.toBeTruthy();
-        expect(makeActions.menuHelpErrorFeedback.getAttribute('disabled')).not.toBeTruthy();
+    //test item "Enviar comentarios" && "Informar de un error"  are enable
+    expect(makeActions.menuHelpComments.getAttribute('disabled')).not.toBeTruthy();
+    expect(makeActions.menuHelpErrorFeedback.getAttribute('disabled')).not.toBeTruthy();
 
-        // Test redirect to faq, forum and help
+    // Test redirect to faq, forum and help
 
-        function _toMatchUrl(url) {
-            browser.sleep(vars.timeToWaitTab);
-            return browser.getAllWindowHandles().then(function(handles) {
-                return browser.switchTo().window(handles[1]).then(function() {
-                    expect(browser.getCurrentUrl()).toMatch(url);
-                    return browser.close().then(function() {
-                        return browser.switchTo().window(handles[0]);
-                    });
-                });
-            });
-        }
+    function _toMatchUrl(url) {
+      browser.sleep(vars.timeToWaitTab);
+      return browser.getAllWindowHandles().then(function(handles) {
+        return browser.switchTo().window(handles[1]).then(function() {
+          expect(browser.getCurrentUrl()).toMatch(url);
+          return browser.close().then(function() {
+            return browser.switchTo().window(handles[0]);
+          });
+        });
+      });
+    }
 
-        makeActions.menuHelpFaq.click().then(function() {
-            browser.sleep(vars.timeToWaitTab);
-            _toMatchUrl(/#\/help/).then(function() {
+    makeActions.menuHelpFaq.click().then(function() {
+      browser.sleep(vars.timeToWaitTab);
+      _toMatchUrl(/#\/help/).then(function() {
 
-                makeActions.menuHelpTutorial.click().then(function() {
-                    browser.sleep(vars.timeToWaitTab);
-                    _toMatchUrl(/#\/help\/tutorial/).then(function() {
+        makeActions.menuHelpTutorial.click().then(function() {
+          browser.sleep(vars.timeToWaitTab);
+          _toMatchUrl(/#\/help\/tutorial/).then(function() {
 
-                        // FOrum not ready yet to test
-                        // makeActions.menuHelpForum.click().then(function() {
-                        //     browser.sleep(vars.timeToWaitTab);
-                        //     _toMatchUrl(/#\/help\/forum/);
-                        // });
+            // FOrum not ready yet to test
+            // makeActions.menuHelpForum.click().then(function() {
+            //     browser.sleep(vars.timeToWaitTab);
+            //     _toMatchUrl(/#\/help\/forum/);
+            // });
 
-                    });
-                });
-
-            });
+          });
         });
 
-        login.logout();
+      });
     });
 
-    //TODO CHECK TOAST
-    it('Login and send comments', function() {
+    login.logout();
+  });
 
-        login.loginWithRandomUser();
-        make.get();
+  //TODO CHECK TOAST
+  it('bba-103:Login and send comments', function() {
 
-        modals.rejectTour();
-        browser.sleep(vars.timeToWaitFadeModals);
+    login.loginWithRandomUser();
+    make.get();
 
-        makeActions.menuHelp.click();
-        makeActions.menuHelpComments.click();
+    modals.rejectTour();
+    browser.sleep(vars.timeToWaitFadeModals);
 
-        modals.sendCommentsName.sendKeys('TestName');
-        modals.sendCommentsTextarea.sendKeys('Hell text area');
-        modals.confirmOnly.click();
+    makeActions.menuHelp.click();
+    makeActions.menuHelpComments.click();
 
-        // TOAST
-        // browser.sleep(500);
-        // expect(element(by.id('modal-comments-done')).isDisplayed()).toBeTruthy();
-        // expect(element(by.id('modal-comments-done')).isPresent()).toBe(true);
+    modals.sendCommentsName.sendKeys('TestName');
+    modals.sendCommentsTextarea.sendKeys('Hell text area');
+    modals.confirmOnly.click();
 
-        login.logout();
-    });
+    // TOAST
+    // browser.sleep(500);
+    // expect(element(by.id('modal-comments-done')).isDisplayed()).toBeTruthy();
+    // expect(element(by.id('modal-comments-done')).isPresent()).toBe(true);
 
-    //TODO CHECK TOAST
-    it('Login and feedback error', function() {
+    login.logout();
+  });
 
-        login.loginWithRandomUser();
-        make.get();
-        modals.rejectTour();
-        browser.sleep(vars.timeToWaitFadeModals);
-        makeActions.menuHelp.click();
-        makeActions.menuHelpErrorFeedback.click();
+  //TODO CHECK TOAST
+  it('bba-104:Login and feedback error', function() {
 
-        modals.notifyErrorTextArea.sendKeys('Text write in text area');
-        modals.notifyErrorSO.sendKeys('Text write S.O Ubuntu ;)');
-        modals.notifyErrorBrowser.sendKeys('Text write browser');
-        modals.confirmOnly.click();
+    login.loginWithRandomUser();
+    make.get();
+    modals.rejectTour();
+    browser.sleep(vars.timeToWaitFadeModals);
+    makeActions.menuHelp.click();
+    makeActions.menuHelpErrorFeedback.click();
 
-        login.logout();
-    });
+    modals.notifyErrorTextArea.sendKeys('Text write in text area');
+    modals.notifyErrorSO.sendKeys('Text write S.O Ubuntu ;)');
+    modals.notifyErrorBrowser.sendKeys('Text write browser');
+    modals.confirmOnly.click();
+
+    login.logout();
+  });
 
 
-    it('NO login "Enviar comentarios" && "Informar de un error" are no displayed ', function() {
+  it('bba-106:NO login, send comment and feedback modal are not displayed but it posible send mailto', function() {
 
-        make.get();
-        modals.attentionContinueGuest.click();
-        modals.rejectTour();
-        browser.sleep(vars.timeToWaitFadeModals);
+    make.get();
+    modals.attentionContinueGuest.click();
+    modals.rejectTour();
+    browser.sleep(vars.timeToWaitFadeModals);
 
-        makeActions.menuHelp.click();
-        ///test item "Enviar comentarios" && "Informar de un error"  are enable
-        expect(makeActions.menuHelpComments.getAttribute('href')).toMatch('mailto:support-bitbloq@bq.com');
-        expect(makeActions.menuHelpErrorFeedback.getAttribute('href')).toMatch('mailto:support-bitbloq@bq.com');
+    makeActions.menuHelp.click();
+    ///test item "Enviar comentarios" && "Informar de un error"  are enable
+    expect(makeActions.menuHelpComments.getAttribute('href')).toMatch('mailto:support-bitbloq@bq.com');
+    expect(makeActions.menuHelpErrorFeedback.getAttribute('href')).toMatch('mailto:support-bitbloq@bq.com');
 
-    });
-
+  });
 
 });
