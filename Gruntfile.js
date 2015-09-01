ef004c297d54c632bed1a5f2dd8b5ce6b355c841
luisangonzalez <luisantonio.gonzalez@bq.com>
2015-09-01 14:59:51 +0200

    add testlinkConnector && draft grunt task && async dev-dependeces
    Change-Id: I4d5f810cd6f6390e054c19acbda1bba65c911f6d

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,186 +1,284 @@
 'use strict';
 /*jshint camelcase: false */
+/* jshint loopfunc: true */
 
 module.exports = function(grunt) {
 
-  var path = require('path');
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+   var path = require('path');
+   var async = require('async');
+   // Load grunt tasks automatically
+   require('load-grunt-tasks')(grunt);
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+   // Time how long tasks take. Can help when optimizing build times
+   require('time-grunt')(grunt);
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+   grunt.initConfig({
+      jshint: {
+         options: {
+            jshintrc: '.jshintrc',
+            reporter: require('jshint-stylish')
+         },
+         all: {
+            src: [
+               'Gruntfile.js',
+               'test/e2e/protractor/**/*.js'
+            ]
+         }
       },
-      all: {
-        src: [
-          'Gruntfile.js',
-          'test/e2e/protractor/**/*.js'
-        ]
+
+      protractor_webdriver: {
+
+         e2eStart: {
+            options: {
+               path: path.resolve() + '/node_modules/protractor/bin/',
+               command: 'webdriver-manager start --standalone'
+
+            }
+         }
+      },
+
+      protractor: {
+         options: { // Default config file
+            configFile: path.resolve() + '/test/e2e/protractor/confs/basic.js', // Default config file
+            keepAlive: false, // If false, the grunt process stops when the test fails.
+            noColor: true // If true, protractor will not use colors in its output.
+         },
+         local: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
+               args: {
+                  baseUrl: grunt.option('target') || 'http://localhost:9000/',
+                  seleniumAddress: 'http://localhost:4444/wd/hub'
+               }
+            }
+         },
+         integration: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
+               args: {
+                  baseUrl: 'http://int-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
+                  seleniumAddress: 'http://localhost:4444/wd/hub'
+               }
+            }
+         },
+         qa: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
+               args: {
+                  baseUrl: 'http://qa-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
+                  seleniumAddress: 'http://localhost:4444/wd/hub'
+               }
+            }
+         },
+         next: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
+               args: {
+                  baseUrl: 'http://next-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
+                  seleniumAddress: 'http://localhost:4444/wd/hub'
+               }
+            }
+         },
+         mvp: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
+               args: {
+                  baseUrl: 'http://mvp-bitbloq.bq.com/',
+                  seleniumAddress: 'http://localhost:4444/wd/hub'
+               }
+            }
+         },
+         staging: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
+               args: {
+                  baseUrl: 'http://staging-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
+                  seleniumAddress: 'http://localhost:4444/wd/hub'
+               }
+            }
+         },
+         saucelabs_integration: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confsf/basic_saucelabs.js',
+               args: {
+                  baseUrl: 'http://int-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
+               }
+            }
+         },
+         saucelabs_qa: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confsf/basic_saucelabs.js',
+               args: {
+                  baseUrl: 'http://qa-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
+               }
+            }
+         },
+         saucelabs_mvp: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confsf/basic_saucelabs.js',
+               args: {
+                  baseUrl: 'http://mvp-bitbloq.bq.com/'
+               }
+            }
+         },
+         saucelabs_integration_all: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confsf/allOS_saucelabs.js',
+               args: {
+                  baseUrl: 'http://int-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
+               }
+            }
+         },
+         saucelabs_qa_all: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confsf/allOS_saucelabs.js',
+               args: {
+                  baseUrl: 'http://qa-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
+               }
+            }
+         },
+         saucelabs_mvp_all: {
+            options: {
+               configFile: path.resolve() + '/test/e2e/protractor/confsf/allOS_saucelabs.js',
+               args: {
+                  baseUrl: 'http://mvp-bitbloq.bq.com/'
+               }
+            }
+         }
       }
-    },
-
-    protractor_webdriver: {
-
-      e2eStart: {
-        options: {
-          path: path.resolve() + '/node_modules/protractor/bin/',
-          command: 'webdriver-manager start --standalone'
-
-        }
-      }
-    },
-
-    protractor: {
-      options: { // Default config file
-        configFile: path.resolve() + '/test/e2e/protractor/confs/basic.js', // Default config file
-        keepAlive: false, // If false, the grunt process stops when the test fails.
-        noColor: true // If true, protractor will not use colors in its output.
-      },
-      local: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
-          args: {
-            baseUrl: grunt.option('target') || 'http://localhost:9000/',
-            seleniumAddress: 'http://localhost:4444/wd/hub'
-          }
-        }
-      },
-      integration: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
-          args: {
-            baseUrl: 'http://int-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
-            seleniumAddress: 'http://localhost:4444/wd/hub'
-          }
-        }
-      },
-      qa: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
-          args: {
-            baseUrl: 'http://qa-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
-            seleniumAddress: 'http://localhost:4444/wd/hub'
-          }
-        }
-      },
-      next: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
-          args: {
-            baseUrl: 'http://next-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
-            seleniumAddress: 'http://localhost:4444/wd/hub'
-          }
-        }
-      },
-      mvp: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
-          args: {
-            baseUrl: 'http://mvp-bitbloq.bq.com/',
-            seleniumAddress: 'http://localhost:4444/wd/hub'
-          }
-        }
-      },
-      staging: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confs/basic_local.js',
-          args: {
-            baseUrl: 'http://staging-bitbloq.com.s3-website-eu-west-1.amazonaws.com/',
-            seleniumAddress: 'http://localhost:4444/wd/hub'
-          }
-        }
-      },
-      saucelabs_integration: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confsf/basic_saucelabs.js',
-          args: {
-            baseUrl: 'http://int-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
-          }
-        }
-      },
-      saucelabs_qa: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confsf/basic_saucelabs.js',
-          args: {
-            baseUrl: 'http://qa-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
-          }
-        }
-      },
-      saucelabs_mvp: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confsf/basic_saucelabs.js',
-          args: {
-            baseUrl: 'http://mvp-bitbloq.bq.com/'
-          }
-        }
-      },
-      saucelabs_integration_all: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confsf/allOS_saucelabs.js',
-          args: {
-            baseUrl: 'http://int-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
-          }
-        }
-      },
-      saucelabs_qa_all: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confsf/allOS_saucelabs.js',
-          args: {
-            baseUrl: 'http://qa-bitbloq.com.s3-website-eu-west-1.amazonaws.com/'
-          }
-        }
-      },
-      saucelabs_mvp_all: {
-        options: {
-          configFile: path.resolve() + '/test/e2e/protractor/confsf/allOS_saucelabs.js',
-          args: {
-            baseUrl: 'http://mvp-bitbloq.bq.com/'
-          }
-        }
-      }
-    }
 
 
-  });
+   });
 
-  grunt.registerTask('default', ['jshint']);
+   grunt.registerTask('default', ['jshint']);
 
-  grunt.registerTask('test', 'Protractor e2e funcional test (Selenium) task.', function(env) {
-    //remember send --saucelabs=true --all or --all argument will be lost
-    var saucelabs = grunt.option('saucelabs'),
-      all = grunt.option('all'),
-      tasks = ['jshint'];
+   grunt.registerTask('test', 'Protractor e2e funcional test (Selenium) task.', function(env) {
+      //remember send --saucelabs=true --all or --all argument will be lost
+      var saucelabs = grunt.option('saucelabs'),
+         all = grunt.option('all'),
+         tasks = ['jshint'];
 
-    env = env || 'local';
+      env = env || 'local';
 
-    if (saucelabs) {
-      if (env === 'local') {
-        grunt.fatal('we can send ' + env + ' to SauceLabs');
+      if (saucelabs) {
+         if (env === 'local') {
+            grunt.fatal('we can send ' + env + ' to SauceLabs');
+         } else {
+            console.log('all=' + all);
+            console.log('saucelabs=' + saucelabs);
+            if (all) {
+
+               tasks.push('protractor:saucelabs_' + env + '_all');
+            } else {
+               tasks.push('protractor:saucelabs_' + env);
+            }
+
+         }
       } else {
-        console.log('all=' + all);
-        console.log('saucelabs=' + saucelabs);
-        if (all) {
-
-          tasks.push('protractor:saucelabs_' + env + '_all');
-        } else {
-          tasks.push('protractor:saucelabs_' + env);
-        }
-
+         tasks.push('protractor_webdriver:e2eStart');
+         tasks.push('protractor:' + env);
       }
-    } else {
-      tasks.push('protractor_webdriver:e2eStart');
-      tasks.push('protractor:' + env);
-    }
 
-    grunt.task.run(tasks);
+      grunt.task.run(tasks);
 
-  });
+   });
 
+   grunt.registerTask('prueba', 'Prueba grunt', function() {
+
+      // Force task into async mode and grab a handle to the "done" function.
+      var done = this.async();
+      // Run some sync stuff.
+      grunt.log.writeln('Processing task...');
+      // And some async stuff.
+
+      //Connecto testlink
+      var TestlinkConnect = require('testlink-connect'),
+         fs = require('fs'),
+         testlinkConnect = new TestlinkConnect('8b4c278f1df8e4059f894acadf9932bb', 'http://testlink.mundoreader.local/lib/api/xmlrpc/v1/xmlrpc.php'),
+         file = './target/e2e/protractor/resultTest.json';
+
+      //Configuration API
+      var testlinkTestPlanID = '28822',
+         testlinkBuildID = '921',
+         testlinkUser = 'luisangonzalez';
+
+      //Obj result
+      var Passed = function() {
+         return {
+            user: testlinkUser,
+            testplanid: testlinkTestPlanID,
+            buildid: testlinkBuildID,
+            testcaseexternalid: '',
+            notes: '',
+            status: 'p',
+            overwrite: true
+         };
+      };
+
+      function reporter(value) {
+         grunt.log.writeln('reporter', value);
+         testlinkConnect.reportTCResult(value, function(callback) {
+            console.log(callback);
+            //callback(null);
+         });
+        
+      }
+
+      //Red json result test && reprotTCResult
+      var obj, passedArray = [];
+
+      function readFile(callback) {
+
+         fs.readFile(file, 'utf8', function(err, data) {
+            grunt.log.writeln('File read');
+            if (err) {
+               throw err;
+            }
+            obj = JSON.parse(data);
+
+            for (var i in obj) {
+
+               var passedItem = new Passed();
+
+               var test = obj[i].description.split(':');
+               passedItem.testcaseexternalid = test[0];
+               if (obj[i].assertions[0].passed) {
+                  passedItem.status = 'p';
+               } else {
+                  passedItem.status = 'f';
+               }
+
+               passedArray.push(passedItem);
+
+            }
+
+            callback(null);
+
+         });
+      }
+
+      async.waterfall([
+         function(callback) {
+            readFile(callback);
+         },function (callback) {
+           async.map(passedArray, reporter, function(err, results) {
+              console.log('Async map finished', results);
+              callback(null);
+           });
+
+         }
+      ], function(err, result) {
+         grunt.log.writeln(result);
+         done();
+      });
+
+      //http://chaitanyaqa.github.io/testlink-api-client/classes/TestLinkApi.html#method_reportTCResult
+      // testlinkConnect.reportTCResult(passed, function(callback) {
+      //    console.log(callback);
+      // });
+
+
+   });
 
 
 };
--- test/e2e/protractor/confs/testlinkConnector.js
+++ test/e2e/protractor/confs/testlinkConnector.js
@@ -3,34 +3,53 @@ Connecto to testlink for upload test result
 */
 
 'use strict';
-
-//Connecto testlink
-var TestlinkConnect = require('testlink-connect'),
-  testlinkConnect = new TestlinkConnect('8b4c278f1df8e4059f894acadf9932bb', 'http://testlink.mundoreader.local/lib/api/xmlrpc/v1/xmlrpc.php');
-
-//Configuration
-
-// dir = '../../../target/e2e/protractor/resultTest.json'
-var testlinkTestPlanID = '28822',
-  testlinkBuildID = '921',
-  testlinkUser = 'luisangonzalez';
-
-//tc.reportTCResult(passed,function(callback){ console.log(callback); });
-
-var passed = {
-  user: testlinkUser,
-  testplanid: testlinkTestPlanID,
-  buildid: testlinkBuildID,
-  testcaseexternalid: 'bba-52',
-  notes: '',
-  status: 'p',
-  overwrite: true
-};
-
-testlinkConnect.reportTCResult(passed, function(callback) {
-  console.log(callback);
-});
+/* jshint loopfunc: true */
 
 
-//bba-52 : Prueba 01 test case
-//http://chaitanyaqa.github.io/testlink-api-client/classes/TestLinkApi.html#method_reportTCResult
+console.log('start');
+   //Connecto testlink
+   var TestlinkConnect = require('testlink-connect'),
+      fs = require('fs'),
+      testlinkConnect = new TestlinkConnect('8b4c278f1df8e4059f894acadf9932bb', 'http://testlink.mundoreader.local/lib/api/xmlrpc/v1/xmlrpc.php'),
+      file = '../../../../target/e2e/protractor/resultTest.json';
+
+   //Configuration API
+   var testlinkTestPlanID = '28822',
+      testlinkBuildID = '921',
+      testlinkUser = 'luisangonzalez';
+
+   //Obj result
+   var passed = {
+      user: testlinkUser,
+      testplanid: testlinkTestPlanID,
+      buildid: testlinkBuildID,
+      testcaseexternalid: '',
+      notes: '',
+      status: 'p',
+      overwrite: true
+   };
+
+   //Red json result test && reprotTCResult
+   var obj;
+   fs.readFile(file, 'utf8', function(err, data) {
+      if (err) {
+         throw err;
+      }
+      obj = JSON.parse(data);
+
+      for (var i in obj) {
+
+         var test = obj[i].description.split(':');
+         passed.testcaseexternalid = test[0];
+         if (obj[i].assertions[0].passed) {
+            passed.status = 'p';
+         } else {
+            passed.status = 'f';
+         }
+         //http://chaitanyaqa.github.io/testlink-api-client/classes/TestLinkApi.html#method_reportTCResult
+         testlinkConnect.reportTCResult(passed, function(callback) {
+            console.log(callback);
+         });
+      }
+
+   });
