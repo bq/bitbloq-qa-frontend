900a0f849b3c5cf87e58af2e04d33abb8aa805eb
Tom Calvo <calvo.tom@gmail.com>
2015-09-22 13:13:48 +0200

    change serial monitor tooltip on mac
    Change-Id: I873eafdf952225ec071b0f12570f6650a30c7b01

--- test/e2e/protractor/tests/bloqsproject/makeActions/makeActions.spec.js
+++ test/e2e/protractor/tests/bloqsproject/makeActions/makeActions.spec.js
@@ -5,140 +5,131 @@
 'use strict';
 
 var Variables = require('../../commons/variables.js'),
-   GlobalFunctions = require('../../commons/globalFunctions.js'),
-   Make = require('../../bloqsproject/make.po.js'),
-   Login = require('../../login/login.po.js'),
-   Modals = require('../../modals/modals.po.js'),
-   MakeActions = require('./makeActions.po.js');
+    GlobalFunctions = require('../../commons/globalFunctions.js'),
+    Make = require('../../bloqsproject/make.po.js'),
+    Login = require('../../login/login.po.js'),
+    Modals = require('../../modals/modals.po.js'),
+    MakeActions = require('./makeActions.po.js');
 
 var vars = new Variables(),
-   globalFunctions = new GlobalFunctions(),
-   login = new Login(),
-   modals = new Modals(),
-   make = new Make(),
-   makeActions = new MakeActions();
+    globalFunctions = new GlobalFunctions(),
+    login = new Login(),
+    modals = new Modals(),
+    make = new Make(),
+    makeActions = new MakeActions();
 
 globalFunctions.xmlReport('makeActions');
 
 describe('check makeActions actions', function() {
 
-   //beforeEach commons
-   globalFunctions.beforeTest();
+    //beforeEach commons
+    globalFunctions.beforeTest();
 
-   // afterEach commons
-   globalFunctions.afterTest();
+    // afterEach commons
+    globalFunctions.afterTest();
 
-   /**
-    * User logged in
-    */
-   it('bba-112:check enabled/disabled options by logged in user', function() {
+    /**
+     * User logged in
+     */
+    it('bba-112:check enabled/disabled options by logged in user', function() {
 
-      login.loginWithRandomUser();
+        login.loginWithRandomUser();
 
-      make.get();
-      modals.rejectTour();
-      browser.sleep(vars.timeToWaitFadeModals);
-      //FILE
-      makeActions.menuFile.click();
-      expect(makeActions.menuNewProject.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuopenProject.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuOpenProjectFromFile.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuChangeName.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuClone.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuDownload.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuExportArduino.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuChangeLanguage.getAttribute('disabled')).not.toBeTruthy();
-      //only enabled when a projects have an id
-      expect(makeActions.removeProject.getAttribute('disabled')).toBeTruthy();
+        make.get();
+        modals.rejectTour();
+        browser.sleep(vars.timeToWaitFadeModals);
+        //FILE
+        makeActions.menuFile.click();
+        expect(makeActions.menuNewProject.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuopenProject.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuOpenProjectFromFile.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuChangeName.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuClone.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuDownload.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuExportArduino.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuChangeLanguage.getAttribute('disabled')).not.toBeTruthy();
+        //only enabled when a projects have an id
+        expect(makeActions.removeProject.getAttribute('disabled')).toBeTruthy();
 
-      //EDIT
-      // makeActions.menuEdit.click();
-      // expect(makeActions.menuEditRedo.getAttribute('disabled')).not.toBeTruthy();
-      // expect(makeActions.menuEditUndo.getAttribute('disabled')).not.toBeTruthy();
+        //EDIT
+        // makeActions.menuEdit.click();
+        // expect(makeActions.menuEditRedo.getAttribute('disabled')).not.toBeTruthy();
+        // expect(makeActions.menuEditUndo.getAttribute('disabled')).not.toBeTruthy();
 
-      //VIEW
-      makeActions.menuView.click();
-      // expect(makeActions.menuViewMoreZoom.getAttribute('disabled')).not.toBeTruthy();
-      // expect(makeActions.menuViewResetZoom.getAttribute('disabled')).not.toBeTruthy();
-      // expect(makeActions.menuViewLessZoom.getAttribute('disabled')).not.toBeTruthy();
+        //VIEW
+        makeActions.menuView.click();
+        // expect(makeActions.menuViewMoreZoom.getAttribute('disabled')).not.toBeTruthy();
+        // expect(makeActions.menuViewResetZoom.getAttribute('disabled')).not.toBeTruthy();
+        // expect(makeActions.menuViewLessZoom.getAttribute('disabled')).not.toBeTruthy();
 
-      //if mac, serial monitor its disabled
-      if (process.platform === 'darwin') {
-         expect(makeActions.menuViewConsole.getAttribute('disabled')).toBeTruthy();
-      } else {
-         expect(makeActions.menuViewConsole.getAttribute('disabled')).not.toBeTruthy();
-      }
+        expect(makeActions.menuViewConsole.getAttribute('disabled')).not.toBeTruthy();
 
-      //SHARE
-      //TODO NEW SPEC PUBLISH AND THEN SHOW PRIVATE
-      makeActions.menuShare.click();
-      expect(makeActions.menuSharePublish.getAttribute('disabled')).toBeTruthy(); //Tiene que estar deshabilitado hasta que se guarde
-      // expect(makeActions.menuShareSocial.getAttribute('disabled')).not.toBeTruthy();
-      //expect(makeActions.menuShareWithUsers.getAttribute('disabled')).not.toBeTruthy();
+        //SHARE
+        //TODO NEW SPEC PUBLISH AND THEN SHOW PRIVATE
+        makeActions.menuShare.click();
+        expect(makeActions.menuSharePublish.getAttribute('disabled')).toBeTruthy(); //Tiene que estar deshabilitado hasta que se guarde
+        // expect(makeActions.menuShareSocial.getAttribute('disabled')).not.toBeTruthy();
+        //expect(makeActions.menuShareWithUsers.getAttribute('disabled')).not.toBeTruthy();
 
-      //HELP
-      makeActions.menuHelp.click();
-      expect(makeActions.menuHelpFaq.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuHelpTutorial.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuHelpComments.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuHelpErrorFeedback.getAttribute('disabled')).not.toBeTruthy();
-      //expect(makeActions.menuHelpForum.getAttribute('disabled')).not.toBeTruthy(); Remove, forum not created yet
+        //HELP
+        makeActions.menuHelp.click();
+        expect(makeActions.menuHelpFaq.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuHelpTutorial.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuHelpComments.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuHelpErrorFeedback.getAttribute('disabled')).not.toBeTruthy();
+        //expect(makeActions.menuHelpForum.getAttribute('disabled')).not.toBeTruthy(); Remove, forum not created yet
 
-      login.logout();
-   });
+        login.logout();
+    });
 
-   /**
-    * User not logged in
-    */
-   it('bba-113:check enabled/disabled options by not logged in user', function() {
+    /**
+     * User not logged in
+     */
+    it('bba-113:check enabled/disabled options by not logged in user', function() {
 
-      make.get();
-      modals.attentionContinueGuest.click();
-      modals.rejectTour();
-      browser.sleep(vars.timeToWaitFadeModals);
+        make.get();
+        modals.attentionContinueGuest.click();
+        modals.rejectTour();
+        browser.sleep(vars.timeToWaitFadeModals);
 
-      //FILE
-      makeActions.menuFile.click();
-      expect(makeActions.menuNewProject.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuopenProject.getAttribute('disabled')).toBeTruthy();
-      expect(makeActions.menuOpenProjectFromFile.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuChangeName.getAttribute('disabled')).toBeTruthy();
-      expect(makeActions.menuClone.getAttribute('disabled')).toBeTruthy();
-      expect(makeActions.menuDownload.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuExportArduino.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuChangeLanguage.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.removeProject.getAttribute('disabled')).toBeTruthy();
+        //FILE
+        makeActions.menuFile.click();
+        expect(makeActions.menuNewProject.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuopenProject.getAttribute('disabled')).toBeTruthy();
+        expect(makeActions.menuOpenProjectFromFile.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuChangeName.getAttribute('disabled')).toBeTruthy();
+        expect(makeActions.menuClone.getAttribute('disabled')).toBeTruthy();
+        expect(makeActions.menuDownload.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuExportArduino.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuChangeLanguage.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.removeProject.getAttribute('disabled')).toBeTruthy();
 
-      //EDIT
-      // makeActions.menuEdit.click();
-      // expect(makeActions.menuEditRedo.getAttribute('disabled')).not.toBeTruthy();
-      // expect(makeActions.menuEditUndo.getAttribute('disabled')).not.toBeTruthy();
+        //EDIT
+        // makeActions.menuEdit.click();
+        // expect(makeActions.menuEditRedo.getAttribute('disabled')).not.toBeTruthy();
+        // expect(makeActions.menuEditUndo.getAttribute('disabled')).not.toBeTruthy();
 
-      //VIEW
-      makeActions.menuView.click();
-      // expect(makeActions.menuViewMoreZoom.getAttribute('disabled')).not.toBeTruthy();
-      // expect(makeActions.menuViewResetZoom.getAttribute('disabled')).not.toBeTruthy();
-      // expect(makeActions.menuViewLessZoom.getAttribute('disabled')).not.toBeTruthy();
-      if (process.platform === 'darwin') {
-         expect(makeActions.menuViewConsole.getAttribute('disabled')).toBeTruthy();
-      } else {
-         expect(makeActions.menuViewConsole.getAttribute('disabled')).not.toBeTruthy();
-      }
+        //VIEW
+        makeActions.menuView.click();
+        // expect(makeActions.menuViewMoreZoom.getAttribute('disabled')).not.toBeTruthy();
+        // expect(makeActions.menuViewResetZoom.getAttribute('disabled')).not.toBeTruthy();
+        // expect(makeActions.menuViewLessZoom.getAttribute('disabled')).not.toBeTruthy();
 
-      //SHARE
-      //TODO NEW SPEC PUBLISH AND THEN SHOW PRIVATE
-      makeActions.menuShare.click();
-      expect(makeActions.menuSharePublish.getAttribute('disabled')).toBeTruthy();
-      // expect(makeActions.menuShareSocial.getAttribute('disabled')).toBeTruthy();
-      //expect(makeActions.menuShareWithUsers.getAttribute('disabled')).toBeTruthy();
+        expect(makeActions.menuViewConsole.getAttribute('disabled')).not.toBeTruthy();
 
-      //HELP
-      makeActions.menuHelp.click();
-      expect(makeActions.menuHelpFaq.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuHelpTutorial.getAttribute('disabled')).not.toBeTruthy();
-      expect(makeActions.menuHelpComments.getAttribute('href')).toMatch('mailto:support-bitbloq@bq.com');
-      //expect(makeActions.menuHelpForum.getAttribute('disabled')).not.toBeTruthy(); Remove, forum not created yet
-   });
+        //SHARE
+        //TODO NEW SPEC PUBLISH AND THEN SHOW PRIVATE
+        makeActions.menuShare.click();
+        expect(makeActions.menuSharePublish.getAttribute('disabled')).toBeTruthy();
+        // expect(makeActions.menuShareSocial.getAttribute('disabled')).toBeTruthy();
+        //expect(makeActions.menuShareWithUsers.getAttribute('disabled')).toBeTruthy();
 
+        //HELP
+        makeActions.menuHelp.click();
+        expect(makeActions.menuHelpFaq.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuHelpTutorial.getAttribute('disabled')).not.toBeTruthy();
+        expect(makeActions.menuHelpComments.getAttribute('href')).toMatch('mailto:support-bitbloq@bq.com');
+        //expect(makeActions.menuHelpForum.getAttribute('disabled')).not.toBeTruthy(); Remove, forum not created yet
+    });
 
 });
