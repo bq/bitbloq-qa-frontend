69cca12fdff8f828d143a680a4c781c67e5c4749
luisangonzalez <luisantonio.gonzalez@bq.com>
2015-12-01 16:09:07 +0100

    add test bba-72
    Change-Id: I82c7cb20c30a718da61281a607aaf5a649d3d4c2

--- test/e2e/protractor/tests/unsupported/unsupportedDesktop.spec.js
+++ test/e2e/protractor/tests/unsupported/unsupportedDesktop.spec.js
@@ -2,11 +2,13 @@
 
 var
     GlobalFunctions = require('../commons/globalFunctions.js'),
-    Unsupported = require('./unsupported.po.js');
+    Unsupported = require('./unsupported.po.js'),
+    Variables = require('../commons/variables.js');
 
 var
     globalFunctions = new GlobalFunctions(),
-    unsupported = new Unsupported();
+    unsupported = new Unsupported(),
+    vars = new Variables();
 
 globalFunctions.xmlReport('unsupportedDesktop');
 
@@ -25,6 +27,25 @@ describe('Check unsupported in desktop', function() {
     it('bba-71:If click on "continuar de todos modos" redirect home page', function() {
         unsupported.continueButton.click();
         expect(browser.getCurrentUrl()).toMatch(browser.baseUrl);
+    });
+
+    it('bba-72:If click on "Descargar Google Chrome" redirect google chrome web', function() {
+        unsupported.downloadGoogleButton.click();
+        browser.sleep(vars.timeToWaitTab);
+        browser.getAllWindowHandles().then(function(handles) {
+            browser.ignoreSynchronization = true;
+            browser.switchTo().window(handles[1]).then(function() {
+                browser.sleep(vars.timeToWaitTab);
+                expect(browser.getCurrentUrl()).toMatch('https://www.google.com/chrome/browser/desktop/index.html');
+                // Check is present CSS button download chrome in google web
+                expect(element(by.css('#marquee > a')).isPresent()).toBe(true);
+                browser.close().then(function() {
+                    browser.switchTo().window(handles[0]);
+                    browser.ignoreSynchronization = false;
+                });
+            });
+        });
+
     });
 
 });
