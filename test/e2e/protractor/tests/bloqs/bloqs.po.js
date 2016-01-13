1dfee9e8ed3a00999a35d4cd48553e4611bba6b5
FdomBQ <fernando.delolmo@bq.com>
2016-01-13 13:24:17 +0100

    xit for test First version of bloqs api for test.
    Change-Id: I93da676627658f1b2248dc8713485f3d093fee89

--- test/e2e/protractor/tests/bloqs/bloqs.po.js
+++ test/e2e/protractor/tests/bloqs/bloqs.po.js
@@ -2,135 +2,299 @@
 
 var Bloqs = function() {
 
-    //var Q = require('q');
-
+    // This function returns the element associated
+    // to the toolbox section passed in param
     this.getToolboxPO = function(section) {
         return $('[data-element="toolbox-' + section + '"]');
     };
 
-    // this.getBloq = function(section, bloqClass) {
+    // This function returns the element associated to the
+    // advanced toolbox button
+    this.getAdvancedButton = function(toolbox) {
+        return toolbox.$('.btn--advanced');
+    };
 
-    //     var toolbox = this.getToolboxPO(section);
-    //     toolbox.click();
-    //     browser.sleep(1000);
-    //     return toolbox.$('.' + bloqClass);
-    // };
+    // This function returns the element associated to each
+    // bloqs section, it can be vars, setup or loop
+    this.getRegion = function(region) {
+        return element(by.css('.bloq-' + region));
+    };
 
-    this.getBloq = function(section, bloqClass) {
+    // This function close the bloqs tab
+    this.closeTab = function() {
+        element(by.css('[id="bloqs--field"]')).click();
+    };
+
+    // This function return the conector passed in params from the bloq header
+    this.getConectorHeader = function(bloque, conector) {
+        return $(bloque.parentElementArrayFinder.locator_.value + ' > .bloq--statement-input__header > [data-content-id="' + conector + '"]');
+    };
+
+    // This function return the conector passed in params from the bloq return
+    this.getConectorReturn = function(bloque, conector) {
+        return $(bloque.parentElementArrayFinder.locator_.value + ' > .bloq--extension > .bloq--extension--end > [data-content-id="' + conector + '"]');
+    };
+
+    // This function return the conector passed in params from the fixed bloq
+    this.getConectorFixed = function(bloque, conector) {
+        return $(bloque.parentElementArrayFinder.locator_.value + ' > .bloq--fixed > [data-content-id="' + conector + '"]');
+    };
+
+    // This function return the conector passed in params from the nested bloq
+    this.getConectorNested = function(bloque, conector) {
+        return $(bloque.parentElementArrayFinder.locator_.value + ' > [data-content-id="' + conector + '"]');
+    };
+
+    // This function set the value or text input from the bloq header
+    this.setValueHeader = function(bloque, tipo, value) {
         var that = this,
-            bloqsuniquetag = Date.now();
+            elem = that.getConectorHeader(bloque, tipo);
 
-        return browser.executeScript('document.querySelectorAll(\'[data-element="toolbox-' + section + '"] .' + bloqClass + '\')[0].setAttribute("bloqsuniquetag", "' + bloqsuniquetag + '")').then(function() {
-            var toolbox = that.getToolboxPO(section);
-            toolbox.click();
-            browser.sleep(1000);
-            return $('[bloqsuniquetag="' + bloqsuniquetag + '"]');
+        elem.clear();
+        elem.sendKeys(value);
+    };
+
+    // This function set the value or text input from the bloq return
+    this.setValueReturn = function(bloque, tipo, value) {
+        var that = this,
+            elem = that.getConectorReturn(bloque, tipo);
+
+        elem.clear();
+        elem.sendKeys(value);
+    };
+
+    // This function set the value or text input from the fixed bloq
+    this.setValueFixed = function(bloque, tipo, value) {
+        var that = this,
+            elem = that.getConectorFixed(bloque, tipo);
+
+        elem.clear();
+        elem.sendKeys(value);
+    };
+
+    // This function set the value or text input from the nested bloq
+    this.setValueNested = function(bloque, tipo, value) {
+        var that = this,
+            elem = that.getConectorNested(bloque, tipo);
+
+        elem.clear();
+        elem.sendKeys(value);
+    };
+
+    // This function select an option from the dropdown from the bloq header
+    this.setDropdownHeader = function(bloque, tipo, operacion) {
+        var that = this,
+            elem = that.getConectorHeader(bloque, tipo);
+
+        elem.click();
+        elem.$('[value="' + operacion + '"]').click();
+    };
+
+    // This function select an option from the dropdown from the bloq return
+    this.setDropdownReturn = function(bloque, tipo, operacion) {
+        var that = this,
+            elem = that.getConectorReturn(bloque, tipo);
+
+        elem.click();
+        elem.$('[value="' + operacion + '"]').click();
+    };
+
+    // This function select an option from the dropdown from the fixed bloq
+    this.setDropdownFixed = function(bloque, tipo, operacion) {
+        var that = this,
+            elem = that.getConectorFixed(bloque, tipo);
+
+        elem.click();
+        elem.$('[value="' + operacion + '"]').click();
+    };
+
+    // This function select an option from the dropdown from the nested bloq
+    this.setDropdownNested = function(bloque, tipo, operacion) {
+        var that = this,
+            elem = that.getConectorNested(bloque, tipo);
+            
+        elem.click();
+        elem.$('[value="' + operacion + '"]').click();
+    };
+
+    // This function connect a bloq into the bloq header
+    this.connectElementHeader = function(arg, bloqueDest, bloqueMover) {
+        var that = this;
+        var conector = that.getConectorHeader(bloqueDest, arg);
+
+        conector.getLocation().then(function(location) {
+            bloqueMover.getLocation().then(function(location2) {
+                var distance = that.getDistance(location, location2);
+                that.moveBloq(bloqueMover, {
+                    x: distance.x + 10,
+                    y: distance.y + 10
+                });
+            });
         });
     };
 
-    /**
-     * Always to a visible position
-     */
+    // This function connect a bloq into the bloq return
+    this.connectElementReturn = function(arg, bloqueDest, bloqueMover) {
+        var that = this;
+        var conector = that.getConectorReturn(bloqueDest, arg);
+
+        conector.getLocation().then(function(location) {
+            bloqueMover.getLocation().then(function(location2) {
+                var distance = that.getDistance(location, location2);
+                that.moveBloq(bloqueMover, {
+                    x: distance.x + 10,
+                    y: distance.y + 10
+                });
+            });
+        });
+    };
+
+    // This function connect a bloq into the fixed bloq
+    this.connectElementFixed = function(arg, bloqueDest, bloqueMover) {
+        var that = this;
+        var conector = that.getConectorFixed(bloqueDest, arg);
+
+        conector.getLocation().then(function(location) {
+            bloqueMover.getLocation().then(function(location2) {
+                var distance = that.getDistance(location, location2);
+                that.moveBloq(bloqueMover, {
+                    x: distance.x + 10,
+                    y: distance.y + 10
+                });
+            });
+        });
+    };
+
+    // This function connect a bloq into the nested bloq
+    this.connectElementNested = function(arg, bloqueDest, bloqueMover) {
+        var that = this;
+        var conector = that.getConectorNested(bloqueDest, arg);
+
+        conector.getLocation().then(function(location) {
+            bloqueMover.getLocation().then(function(location2) {
+                var distance = that.getDistance(location, location2);
+                that.moveBloq(bloqueMover, {
+                    x: distance.x + 10,
+                    y: distance.y + 10
+                });
+            });
+        });
+    };
+
+    // This function add a unique tag to a bloq in the class and section passed
+    // in params and return the tag.
+    this.getBloq = function(section, bloqClass, advanced) {
+        var that = this,
+            bloqsuniquetag = Date.now(),
+            aleatorio = (Math.round(Math.random() * 100)) + 0;
+
+        return browser.executeScript('document.querySelectorAll(\'[data-element="toolbox-' + section + '"] .' + bloqClass + '\')[0].setAttribute("bloqsuniquetag", "' + bloqsuniquetag + aleatorio + '")').then(function() {
+            var toolbox = that.getToolboxPO(section);
+            toolbox.click();
+            if (advanced) {
+                that.getAdvancedButton(toolbox).click();
+            }
+            browser.sleep(1000);
+            return $('[bloqsuniquetag="' + bloqsuniquetag + aleatorio + '"]');
+        });
+    };
+
+    // This function return the distance between two position
+    // objects.
+    this.getDistance = function(position1, position2) {
+        return {
+            x: position1.x - position2.x,
+            y: position1.y - position2.y
+        };
+    };
+
+    // This function move a bloq
     this.moveBloq = function(bloq, positionDiff) {
         browser.actions()
-            .mouseMove(bloq.getWebElement(), {
+            .mouseMove(bloq, {
                 x: 10,
                 y: 10
             })
             .mouseDown()
-            .mouseMove(bloq.getWebElement()) //premove start
-            .mouseMove({ //first move to get distance and leave preMove status
+            .mouseMove(bloq, {
                 x: 10,
                 y: 10
-            })
-            .mouseMove({ //first move to get distance and leave preMove status
+            }) //premove start
+            .mouseMove(bloq, {
                 x: 0,
                 y: 0
             })
-            .mouseMove(positionDiff)
+            .mouseMove(positionDiff, {
+                x: 0,
+                y: 0
+            })
             .mouseUp()
             .perform();
     };
 
-    this.connectBloqsDown = function() {
+    // This function moves a bloq to the zone passed in params
+    // it can be vars, setup or loop
+    this.addToGroup = function(zona, bloque) {
         var that = this;
+        var group = that.getRegion(zona);
 
-        that.getBloq('functions', 'bloq-void-function').then(function(voidFunctionBloq) {
-
-            that.moveBloq(voidFunctionBloq, {
-                x: -100,
-                y: 0
-            });
-
-            that.getBloq('functions', 'bloq-void-function').then(function(voidFunctionBloq2) {
-
-                that.moveBloq(voidFunctionBloq2, {
-                    x: 0,
-                    y: 100
+        group.getLocation().then(function(location) {
+            bloque.getLocation().then(function(location2) {
+                var distance = that.getDistance(location, location2);
+                that.moveBloq(bloque, {
+                    x: distance.x + 10,
+                    y: distance.y + 100
                 });
             });
         });
     };
 
-    this.connectBloqsUp = function() {
+    // This function connect two boqs through the conector passed in params
+    // it can be up, down or root
+    this.connectBloqs = function(conector, bloqueDest, bloqueMover) {
         var that = this;
 
-        that.getBloq('functions', 'bloq-void-function').then(function(voidFunctionBloq) {
-
-            that.moveBloq(voidFunctionBloq, {
-                x: 0,
-                y: 100
-            });
-
-            that.getBloq('functions', 'bloq-void-function').then(function(voidFunctionBloq2) {
-
-                that.moveBloq(voidFunctionBloq2, {
-                    x: -100,
-                    y: 0
+        if (conector === 'up') {
+            bloqueDest.getSize().then(function(size) {
+                bloqueDest.getLocation().then(function(location) {
+                    bloqueMover.getLocation().then(function(location2) {
+                        var distance = that.getDistance(location, location2);
+                        that.moveBloq(bloqueMover, {
+                            x: distance.x,
+                            y: distance.y + size.height
+                        });
+                    });
                 });
             });
-        });
-    };
-
-    this.connectBloqsRoot = function() {
-        var that = this;
-
-        that.getBloq('functions', 'bloq-void-function').then(function(voidFunctionBloq) {
-
-            that.moveBloq(voidFunctionBloq, {
-                x: 0,
-                y: 0
-            });
-
-            that.getBloq('functions', 'bloq-void-function').then(function(voidFunctionBloq2) {
-
-                that.moveBloq(voidFunctionBloq2, {
-                    x: 0,
-                    y: 50
+        } else if (conector === 'down') {
+            bloqueDest.getSize().then(function() {
+                bloqueDest.getLocation().then(function(location) {
+                    bloqueMover.getLocation().then(function(location2) {
+                        var distance = that.getDistance(location, location2);
+                        that.moveBloq(bloqueMover, {
+                            x: distance.x,
+                            y: distance.y - 10
+                        });
+                    });
                 });
             });
-        });
-    };
-
-    this.connectElement = function() {
-        var that = this;
-
-        that.getBloq('vars', 'bloq-declare-variable').then(function(voidFunctionBloq) {
-
-            that.moveBloq(voidFunctionBloq, {
-                x: -200,
-                y: 0
-            });
-
-            that.getBloq('maths', 'bloq-number').then(function(voidFunctionBloq2) {
-
-                that.moveBloq(voidFunctionBloq2, {
-                    x: 100,
-                    y: 0
+        } else if (conector === 'root') {
+            bloqueDest.getSize().then(function(size) {
+                bloqueDest.getLocation().then(function(location) {
+                    bloqueMover.getLocation().then(function(location2) {
+                        var distance = that.getDistance(location, location2);
+                        that.moveBloq(bloqueMover, {
+                            x: distance.x,
+                            y: distance.y + size.height / 2
+                        });
+                    });
                 });
             });
-        });
+        } else {
+            console.log('no way');
+        }
     };
+
 };
 
 module.exports = Bloqs;
--- test/e2e/protractor/tests/bloqs/bloqs.spec.js
+++ test/e2e/protractor/tests/bloqs/bloqs.spec.js
@@ -3,11 +3,13 @@
 var GlobalFunctions = require('../commons/globalFunctions.js'),
     Make = require('../bloqsproject/make.po.js'),
     Modals = require('../modals/modals.po.js'),
+    Variables = require('../commons/variables.js'),
     Bloqs = require('./bloqs.po.js');
 
 var globalFunctions = new GlobalFunctions(),
     make = new Make(),
     modals = new Modals(),
+    vars = new Variables(),
     bloqs = new Bloqs();
 
 globalFunctions.xmlReport('bloqs');
@@ -20,17 +22,80 @@ describe('bloqs', function() {
     // afterEach commons
     globalFunctions.afterTest();
 
-    xit('check bloqs connections', function() {
+    it('check api', function() {
         //login.loginWithRandomUser();
         make.get();
 
         modals.attentionContinueGuest.click();
-
+        browser.sleep(vars.timeToWaitFadeModals);
         modals.rejectTour();
+        browser.sleep(vars.timeToWaitFadeModals);
         make.softwareTab.click();
+        browser.sleep(vars.timeToWaitTab);
 
-        bloqs.connectElement();
+        var advanced = true;
+
+        bloqs.getBloq('functions', 'bloq-return-function', !advanced).then(function(bloque1) {
+            bloqs.addToGroup('vars', bloque1);
+            bloqs.closeTab();
+            bloqs.getBloq('maths', 'bloq-random', !advanced).then(function(bloque2) {
+                bloqs.connectElementReturn('RETURN', bloque1, bloque2);
+                bloqs.closeTab();
+                bloqs.getBloq('maths', 'bloq-random', !advanced).then(function(bloque3) {
+                    bloqs.connectElementNested('ARG1', bloque2, bloque3);
+                    bloqs.closeTab();
+                    bloqs.getBloq('maths', 'bloq-number', !advanced).then(function(bloque5) {
+                        bloqs.connectElementNested('ARG1', bloque3, bloque5);
+                        bloqs.closeTab();
+                    });
+                    bloqs.getBloq('maths', 'bloq-random', !advanced).then(function(bloque4) {
+                        bloqs.connectElementNested('ARG2', bloque2, bloque4);
+                        bloqs.closeTab();
+                        bloqs.getBloq('maths', 'bloq-number', !advanced).then(function(bloque6) {
+                            bloqs.connectElementNested('ARG2', bloque4, bloque6);
+                            bloqs.closeTab();
+                        });
+                    });
+                    bloqs.getBloq('maths', 'bloq-number', !advanced).then(function(bloque9) {
+                        bloqs.connectElementNested('ARG2', bloque3, bloque9);
+                        bloqs.closeTab();
+                    });
+                });
+            });
+        });
+
+        bloqs.getBloq('vars', 'bloq-declare-variable', !advanced).then(function(bloque1) {
+            bloqs.addToGroup('setup', bloque1);
+            bloqs.closeTab();
+            bloqs.getBloq('maths', 'bloq-random', !advanced).then(function(bloque2) {
+                bloqs.connectElementFixed('VALUE', bloque1, bloque2);
+                bloqs.closeTab();
+                bloqs.getBloq('maths', 'bloq-random', !advanced).then(function(bloque3) {
+                    bloqs.connectElementNested('ARG1', bloque2, bloque3);
+                    bloqs.closeTab();
+                    bloqs.getBloq('maths', 'bloq-number', !advanced).then(function(bloque6) {
+                        bloqs.connectElementNested('ARG2', bloque2, bloque6);
+                        bloqs.closeTab();
+                    });
+                    bloqs.getBloq('control', 'bloq-if', !advanced).then(function(bloque8) {
+                        bloqs.connectBloqs('down', bloque1, bloque8);
+                        bloqs.closeTab();
+                        bloqs.getBloq('control', 'bloq-millis', !advanced).then(function(bloque6) {
+                            bloqs.connectElementHeader('ARG1', bloque8, bloque6);
+                            bloqs.closeTab();
+                        });
+                        bloqs.getBloq('maths', 'bloq-number', !advanced).then(function(bloque9) {
+                            bloqs.connectElementHeader('ARG2', bloque8, bloque9);
+                            bloqs.closeTab();
+                        });
+                    });
+                    bloqs.getBloq('maths', 'bloq-number', !advanced).then(function(bloque9) {
+                        bloqs.connectElementNested('ARG2', bloque3, bloque9);
+                        bloqs.closeTab();
+                    });
+                });
+            });
+        });
         browser.pause();
     });
-
 });
