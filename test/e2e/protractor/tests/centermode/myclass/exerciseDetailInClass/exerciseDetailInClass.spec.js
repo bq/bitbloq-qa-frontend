5dfe4efd0b9ba7c2ee8c0b21ce53874dcc18437d
Tom Calvo <calvo.tom@gmail.com>
2017-08-28 10:58:12 +0200

    added id to test bbb-652

--- test/e2e/protractor/tests/centermode/myclass/exerciseDetailInClass/exerciseDetailInClass.spec.js
+++ test/e2e/protractor/tests/centermode/myclass/exerciseDetailInClass/exerciseDetailInClass.spec.js
@@ -33,7 +33,7 @@ describe('Exercise Detail inside class Detail', function () {
     // afterEach commons
     globalFunctions.afterTest();
 
-    it('bbb-XXX:exerciseDetailInClass: correct exercise', function () {
+    it('bbb-652:exerciseDetailInClass: correct exercise', function () {
 
         protractor.promise.all([
             centermode.createHeadMaster({
