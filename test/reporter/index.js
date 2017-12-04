e2ef3123bfef1fb7a0afd85b8aeb562ccb1eee8a
Alvaro Almendros Gala <alvaroalmendros@me.com>
2017-12-04 17:48:18 +0100

    [SWBIT-2727] Layout beta

--- test/reporter/index.js
+++ test/reporter/index.js
@@ -1,41 +1,128 @@
-var Metalsmith      = require('metalsmith');
-var collections     = require('metalsmith-collections');
-var markdown        = require('metalsmith-markdown');
-var layouts         = require('metalsmith-layouts');
-var permalinks      = require('metalsmith-permalinks');
-var pug             = require('metalsmith-pug');
-var sass            = require('metalsmith-sass');
-var models          = require('metalsmith-models');
-var serve           = require('metalsmith-serve');
+'use strict';
 
-Metalsmith(__dirname)
+var fs = require('fs'),
+  xml2js = require('xml2js'),
+  moment =  require('moment');
+
+var ms = require('metalsmith'),
+  autocollections = require('metalsmith-auto-collections'),
+  markdown = require('metalsmith-markdown'),
+  layouts = require('metalsmith-layouts'),
+  permalinks = require('metalsmith-permalinks'),
+  pug = require('metalsmith-pug'),
+  sass = require('metalsmith-sass'),
+  serve = require('metalsmith-serve'),
+  paths = require('metalsmith-paths'),
+  watch = require('metalsmith-watch'),
+  dateFormatter = require('metalsmith-date-formatter');
+
+// xml2js
+var parser = new xml2js.Parser();
+fs.readdir('./target/report/', function(err, data) {
+  if (!err && data) {
+    data.forEach(function(fileName) {
+      fs.readFile('./target/report/' + fileName, function(err, file) {
+        parser.parseString(file, function(err, result) {
+          if (!err && result) {
+            var report = JSON.stringify(result);
+            var reportPretty = JSON.stringify(result, null, 2);
+            // folder
+            var folderName = fileName.replace('.xml', '');
+            fs.readdir(__dirname+'/src/items/', function(err, data) {
+              // existe?
+              if (!err && data && data.indexOf(folderName) === -1) {
+                fs.mkdirSync(__dirname+'/src/items/'+folderName)
+              }
+            });
+            // md
+            var mdDate = moment().format('YYYY-MM-DD').toString()
+            var mdFileName = mdDate+'.'+folderName+'.md';
+            var mdRoute = __dirname+'/src/items/'+folderName+'/'+mdFileName;
+            console.log(folderName+'/'+mdFileName);
+            var output = '---\n';
+            output += 'title: '+ folderName +'\n';
+            output += 'date: '+ mdDate +'\n';
+            output += 'layout: post.jade\n';
+            output += '---\n\n';
+            output += '<h4>JSON Original</h4>\n<pre><code class=\"language-json\">\n'+reportPretty+'\n</code></pre>';
+            console.log(output,'->', mdRoute);
+            fs.writeFile(mdRoute, output, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
+          } else {
+            console.log('[ERROR]: no se ha podido parsear '+file);
+          }
+        });
+      });
+    });
+  } else {
+    console.log('[ERROR]: No hay reportes en ./target/report');
+  }
+});
+
+// metalsmith
+ms(__dirname)
   .metadata({
     title: 'Bitbloq QA Reporter',
     description: 'Static Site generated to display QA reports'
   })
   .source('./src')
   .destination('./dist')
+  .use(
+    watch({
+      paths: {
+        '${source}/**/*': true
+      },
+      livereload: true
+    })
+  )
   .clean(true)
-  .use(collections({
-      'reports': {
-        sortBy : 'date',
-        metadata: {
-            name: 'Reports',
-            description: 'by date'
-        }
+  .use(paths({ property: 'paths' }))
+  .use(
+    autocollections({
+      pattern: 'items/**/*.md',
+      settings: {
+        sortBy: 'date',
+        reverse: true
       }
-    }))
+    })
+  )
   .use(markdown())
-  .use(permalinks({ relative: false, 'pattern': ':date/:title' }))
-  // .use(models({ directory: 'models' }))
-  .use(layouts({
-        engine    : 'pug',
-        directory : './layouts'
-      }))
-  // .use(sass({ outputStyle: 'expanded' }))
+  .use(
+    dateFormatter({
+      dates: [
+        {
+          key: 'date',
+          format: 'DD-MM-YYYY'
+        }
+      ]
+    })
+  )
+  .use(
+    permalinks({
+      relative: false,
+      pattern: ':collection/:url'
+    })
+  )
+  .use(
+    layouts({
+      engine: 'pug',
+      directory: './src/layouts'
+    })
+  )
+  .use(
+    sass({
+      outputStyle: 'expanded',
+      sourceMap: true,
+      sourceMapContents: true
+    })
+  )
   .use(pug({ pretty: false }))
   .use(serve())
   .build(function(err, files) {
-    console.log('HEVEN OR HELL', files);
-    if (err) { throw err; }
+    if (err) {
+      throw err;
+    }
   });
---
+++ test/reporter/src/items/test2/first-post.md
@@ -0,0 +1,7 @@
+---
+title: modalsChangeProjectName
+date: 2017-12-04
+layout: post.jade
+---
+
+An interesting post about how it's going to be different this time around. I'm going write a lot more nowadays and use this blog to improve my writing.
