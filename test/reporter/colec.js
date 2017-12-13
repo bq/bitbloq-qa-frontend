615319c00ae4132c9ca4b9e3642e19b30d4c9eee
alvaro.font <alvaro.font@bq.com>
2017-12-13 11:45:29 +0100

    reporter

---
+++ test/reporter/colec.js
@@ -0,0 +1,222 @@
+module.exports = { colec: {
+  "account": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "accountLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "accountLocalXit": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "autosave": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "autosaveLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsproject": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectHardwareLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectInfo": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectMakeActions": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectMakeActionsEdit": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectMakeActionsFile": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectMakeActionsHelp": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectMakeActionsHelpXit": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectMakeActionsShare": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "bloqsprojectWalkthrough": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "centermode": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "changelog": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "classDetail": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "codeProject": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "codeProjectMakeActions": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "cookiesBar": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "exerciseDetailInClass": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "exercises": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "explore": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "explorefiltersLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "exploreProject": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "exploreXit": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "faq": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "forum": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "forumXit": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "header": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "infotabLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "landing": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "language": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "learn": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "licenses": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "login": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "loginLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "makeactionsEditLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "makeActionsFileLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "makeActionsShareEnv": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "makeactionsShareLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "modalsChangeProjectName": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "mycenter": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "myclass": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "MyExercises": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "myprojects": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "myProjectsLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "projectLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "projects": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "register": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "socialLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "state": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "stateLocal": {
+    "sortBy": "date",
+    "reverse": true
+  },
+  "studentDetailInClass": {
+    "sortBy": "date",
+    "reverse": true
+  }
+} };
